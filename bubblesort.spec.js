3106a556e6463419368a9fef51d31bc7fbcc15c2
Oczane <oczane.rivera@gmail.com>
2017-11-02 15:06:35 -0400

    Test

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -28,11 +28,11 @@ describe('Bubble Sort', function(){
 
 
 
-// beforeAll(function () {
-//   spyOn(tootsiepop, 'lick').and.callThrough(); // replace existing `tootsiepop['lick']` method
-// });
-// it('getting to the center of tootsiepop involves exactly three licks', function () {
-//   tootsiepop.getToCenter();
-//   expect(tootsiepop.lick.calls.count()).toEqual(3);
-// });
-// ...
+beforeAll(function () {
+  spyOn(tootsiepop, 'lick').and.callThrough(); // replace existing `tootsiepop['lick']` method
+});
+it('getting to the center of tootsiepop involves exactly three licks', function () {
+  tootsiepop.getToCenter();
+  expect(tootsiepop.lick.calls.count()).toEqual(3);
+});
+...
